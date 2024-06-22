28472e0d49b3ee95033d9eb3d69a039ba79f0305
Nahuel Carracedo <141567811+ATILA2001@users.noreply.github.com>
2024-06-22 14:44:44 -0300

    Update GUIA.sql

--- GUIA.sql
+++ GUIA.sql
@@ -4,6 +4,7 @@ CREATE VIEW NombreVista AS
 SELECT columnas
 FROM tablas
 WHERE condiciones;
+--
 
 --2. Procedimientos Almacenados
 CREATE PROCEDURE NombreProcedimiento
@@ -12,14 +13,14 @@ AS
 BEGIN
     -- Sentencias SQL
 END;
+--
 
 --3. Transacciones
 BEGIN TRANSACTION;
-
 UPDATE Cuentas SET Saldo = Saldo - 100 WHERE ID = 1;
 INSERT INTO RegistroTransacciones (IDCuenta, Monto, Fecha) VALUES (1, -100, GETDATE());
-
 COMMIT;
+--
 
 --4. Triggers
 CREATE TRIGGER NombreTrigger
@@ -29,3 +30,4 @@ AS
 BEGIN
     -- Lógica del trigger
 END;
+--
